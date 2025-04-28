926391e79c7586e9fa56822753c2dd49cae508da
LucasBenini1 <lucasbenini.11@gmail.com>
2025-04-28 14:02:26 -0300

    Finalizacao do projeto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// Importamos o Bootstrap aqui
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
